7c7b3f63098c2bc01fd1e8f46786dd1231b755b8
Andrey Fontoura <andreyfontoura95@gmail.com>
2025-08-15 16:55:55 -0300

    chore: fix tailwindcss

---
+++ .stylelintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-tailwindcss"
+  ],
+  rules: {
+    "at-rule-no-unknown": null,
+    "no-empty-source": null
+  }
+};

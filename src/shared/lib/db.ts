98d7b3b05ec56ec87c4c15288567aebe1fd1be33
Andrey Fontoura <andreyfontoura95@gmail.com>
2025-08-18 01:18:43 -0300

    remove debug code

--- src/shared/lib/db.ts
+++ src/shared/lib/db.ts
@@ -1,17 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 
-async function debug() {
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-const prisma = new PrismaClient();
-await prisma.$connect();
-const tables = await prisma.$queryRaw<Array<{ table_name: string }>>`SELECT table_name FROM information_schema.tables WHERE table_schema='public';`;
-console.log('Tables in public schema:', tables);
-}
-
-debug();
-
-
 export const database = new PrismaClient().$extends({
   query: {
     user: {
